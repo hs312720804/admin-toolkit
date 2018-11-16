ca76a0007e52d535cc0e0a86ec0123b4e0d682b7
William17 <liangweiliang@coocaa.com>
2018-11-16 18:53:18 +0800

    fix: wrapService

--- src/lib/utils/wrapService.js
+++ src/lib/utils/wrapService.js
@@ -2,25 +2,27 @@ import { Notification } from 'element-ui'
 function wrapService(service) {
     const $service = {}
     Object.keys(service).forEach((key) => {
-        $service[key] = async (args, message) => {
-            return service[key](args)
-                .then((result) => {
-                    if (message) {
-                        Notification({
-                            title: '操作成功',
-                            type: 'success',
-                            message
-                        })
-                    }
-                    return result
-                })
-                .catch((error) => {
-                    Notification({
-                        title: '操作失败',
-                        type: 'error',
-                        message: error.message
+        if (typeof $service[key] === 'function') {
+            $service[key] = async (args, message) => {
+                return service[key](args)
+                    .then((result) => {
+                        if (message) {
+                            Notification({
+                                title: '操作成功',
+                                type: 'success',
+                                message
+                            })
+                        }
+                        return result
                     })
-                })
+                    .catch((error) => {
+                        Notification({
+                            title: '操作失败',
+                            type: 'error',
+                            message: error.message
+                        })
+                    })
+            }
         }
     })
     return $service
