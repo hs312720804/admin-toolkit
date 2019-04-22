c00d81bd0b7a2f2806a1bd125cd65750879d75d2
392241868 <392241868@qq.com>
2019-04-22 14:27:22 +0800

    src/lib/components/ContentWrapper.vue 分页条数加上5条

--- src/lib/components/ContentWrapper.vue
+++ src/lib/components/ContentWrapper.vue
@@ -3,7 +3,7 @@ import GateSchemaForm from './GateSchemaForm'
 import { Pagination } from 'element-ui'
 const defaultPaginationProps = {
   background: true,
-  pageSizes: [5,10, 20, 50, 100],
+  pageSizes: [5, 10, 20, 50, 100],
   pageSize: 10,
   layout: 'total, prev, pager, next, sizes, jumper'
 }
