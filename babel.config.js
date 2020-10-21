ef96270b46426d4d45203fc23325a54b7f5ea59d
王海花 <wanghaihua@wanghaihuadeMacBook-Pro.local>
2020-10-21 17:05:37 +0800

    feat: 单元测试配置

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   presets: [
-    '@vue/app'
-  ]
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current'
+        }
+      }
+    ]
+  ],
+  plugins: ['transform-es2015-modules-commonjs']
 }
